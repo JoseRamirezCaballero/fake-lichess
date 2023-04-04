cc4ec25a2256553d4c78c5717c85b56133cda40a
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-04-04 17:52:04 -0600

    tableros/about debajo del contenido principal

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,11 @@ $numeroJugadores = document.querySelector('#numeroJugadores')
 $numeroPartidas = document.querySelector('#numeroPartidas') 
 
 function actualizarNumeros() {
-    const jugadores = Math.floor(Math.random() * 10000) + 10000;
-    const partidas = Math.floor(Math.random() * 10000) + 21000;
+  const jugadores = Math.floor(Math.random() * 10000) + 10000;
+  const partidas = Math.floor(Math.random() * 10000) + 21000;
 
-    $numeroJugadores.innerHTML = jugadores;
-    $numeroPartidas.innerHTML = partidas;
-  }
+  $numeroJugadores.innerHTML = jugadores;
+  $numeroPartidas.innerHTML = partidas;
+}
 
-  setInterval(actualizarNumeros, 1000);
+setInterval(actualizarNumeros, 1000);

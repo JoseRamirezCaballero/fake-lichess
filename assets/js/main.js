4689f2f951c73a9e10e91cbba9e35ebee67362f6
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-04-03 21:00:07 -0600

    agregue simulacion de jugadores/partidas con js

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$numeroJugadores = document.querySelector('#numeroJugadores') 
+$numeroPartidas = document.querySelector('#numeroPartidas') 
+
+function actualizarNumeros() {
+    const jugadores = Math.floor(Math.random() * 10000) + 10000;
+    const partidas = Math.floor(Math.random() * 10000) + 21000;
+
+    $numeroJugadores.innerHTML = jugadores;
+    $numeroPartidas.innerHTML = partidas;
+  }
+
+  setInterval(actualizarNumeros, 1000);
